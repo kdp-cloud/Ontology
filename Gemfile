525b8c1ccdd75c56f75a4dec268ed2cfd437707a
Kevin De Pelseneer <kevin.depelseneer@gmail.com>
2022-11-05 20:19:23 +0100

    Built gemfile

--- Gemfile
+++ Gemfile
@@ -10,3 +10,15 @@ gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 
 gem "rubocop", "~> 1.21"
+
+gem "uri", "~> 0.11.0"
+
+gem "net", "~> 0.3.3"
+
+gem "json", "~> 2.6"
+
+gem "colorize", "~> 0.8.1"
+
+gem "csv", "~> 3.2"
+
+gem "Ontology", git: "https://github.com/kdp-cloud/Ontology.git"
--- spec/Ontology_spec.rb
+++ spec/Ontology_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe Ontology do
     expect(Ontology::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "should return a HTTP status code 200" do
+    expect(OntologyApi.get_ontology_by_id("efo").is_a?(Net::HTTPSuccess)).to eq(true)
+  end
+
+  it "Should raise an error and return 'nil' when entering a non existing identifier" do
+    expect(OntologyApi.get_ontology_by_id("something")).to eq(nil)
+  end
+  puts OntologyApi.get_ontology_by_id(5).inspect
+  it "Should raise an error when entering a number instead of a string" do
+    expect(OntologyApi.get_ontology_by_id(5)).to eq(nil)
   end
 end
