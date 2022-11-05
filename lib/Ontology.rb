5f35168792973fe36b82f69d0115c0938cab5813
Kevin De Pelseneer <kevin.depelseneer@gmail.com>
2022-11-05 16:41:18 +0100

    Initial commit

---
+++ lib/Ontology.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Ontology/version"
+
+module Ontology
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/Ontology_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ontology do
+  it "has a version number" do
+    expect(Ontology::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
